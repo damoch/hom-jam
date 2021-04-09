d906ad01a834024ef3acdc8e1c36b7690984f298
Damian Ochramowicz <damochramowicz260@gmail.com>
2021-04-09 17:23:27 +0200

    #19 - custom music loader

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Assets.Scripts
 {
@@ -13,15 +19,66 @@ namespace Assets.Scripts
         private AudioSource _audioSource;
         private float _baseVolume;
         private float _volumeDropPerSecond;
+        private List<AudioClip> _customMusic;
+        private int _currentTrackIndex;
+
+        public string MusicFolderName;
+        public string AllowedExtensionsPattern;
 
         private void Awake()
         {
             _audioSource = GetComponent<AudioSource>();
         }
 
-        private void Start()
+        private async void TryLoadAudioFile()
+        {
+            _customMusic = new List<AudioClip>();
+            var path = Path.Combine(Application.dataPath, MusicFolderName);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                return;
+            }
+
+            var files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                var ext = file.Split('.').Last();
+                if(ext.ToLower() != AllowedExtensionsPattern)
+                {
+                    continue;
+                }
+                AudioClip clip = null;
+                using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(file, AudioType.OGGVORBIS))
+                {
+                    uwr.SendWebRequest();
+
+                    // wrap tasks in try/catch, otherwise it'll fail silently
+                    try
+                    {
+                        while (!uwr.isDone) await Task.Delay(5);
+
+                        if (uwr.isNetworkError || uwr.isHttpError) Debug.Log($"{uwr.error}");
+                        else
+                        {
+                            clip = DownloadHandlerAudioClip.GetContent(uwr);
+                            _customMusic.Add(clip);
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        Debug.Log($"{err.Message}, {err.StackTrace}");
+                    }
+                }
+            }
+        }
+
+        private async void Start()
         {
             _baseVolume = _audioSource.volume;
+            TryLoadAudioFile();
         }
 
         private void Update()
@@ -37,7 +94,18 @@ namespace Assets.Scripts
 
         public void PlayInGameMusic()
         {
-            _audioSource.clip = InGameMusic;
+            if(_customMusic.Count > 0)
+            {
+                if(_currentTrackIndex > _customMusic.Count - 1)
+                {
+                    _currentTrackIndex = 0;
+                }
+                _audioSource.clip = _customMusic[_currentTrackIndex++];
+            }
+            else
+            {
+                _audioSource.clip = InGameMusic;
+            }
             _audioSource.Play();
         }
     }
